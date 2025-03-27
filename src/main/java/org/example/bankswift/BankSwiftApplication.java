5b7dc9e6b14d3ae7319afcb8ef7c4213bc90f6ba
Fruzi1k <artem.grun2004pl@gmail.com>
2025-03-27 23:50:56 +0100

    Initial commit

---
+++ src/main/java/org/example/bankswift/BankSwiftApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankswift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSwiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSwiftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bankswift/BankSwiftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankswift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSwiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
